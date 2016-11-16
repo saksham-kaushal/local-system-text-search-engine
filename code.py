c66d87a04e4b7fb0d3c31c2a7634e6caf9fedad0
rns2ra <saksham>
2016-11-17 01:14:59 +0530

    Implemented functions. Added minor details.

--- code.py
+++ code.py
@@ -1,28 +1,51 @@
 import glob
 import re
 
-path=raw_input("Enter path (format- /path/to/folder): ")
-file_list = glob.glob(path+"/*.txt")
-print file_list
+
+def enter_path():
+    """
+    Takes directory location as input and returns a list of files available in
+    the directory.
+    """
+    path=raw_input("Enter path (format- /path/to/folder): ")
+    file_list = glob.glob(path+"/*.txt")
+    #enlists only the contents of directory, not the subdirectories-scope of improvement using regex or glob
+    return file_list
 
 
-word_list= list()
-hashtable= dict()
-for each_file in file_list:
-    with open(each_file,'r') as file_handle:
-        for line in file_handle:
-            wlist= re.findall(r"[\w']+",line)
-            word_list.append(wlist)
-        word_list=[x.lower() for word in word_list for x in word if len(x)>=3]
-        docid = id(word_list)
+def create_hash(file_list):
+    """
+    Creates inverted index of format {word:{documentID:[indeces of positions]}}
+    """
+
+    hashtable= dict()
+    fileID_names= dict()
+
+    for each_file in file_list:
+        with open(each_file,'r') as file_handle:
+            word_list= list()
+            for line in file_handle:
+                wlist= re.findall(r"[\w']+",line)
+                                #removes punctuation except ' and _
+                word_list.append(wlist)
+            word_list=[word.lower() for elements in word_list for word in elements if len(word)>=3]
+                                #removing a, an, as, in etc.
+            docid = id(word_list)
+            fileID_names[docid]=each_file
 
 
-        for each_word in word_list:
-            doc=dict()
-            doc[docid]=[i for i, j in enumerate(word_list) if j == each_word]
-            if each_word not in hashtable.keys():
-                hashtable[each_word]=doc
-            else:
-                hashtable[each_word][docid]=doc[docid]
+            for each_word in word_list:
+                doc=dict()
+                doc[docid]=[i for i, j in enumerate(word_list) if j == each_word]
+                if each_word not in hashtable.keys():
+                    hashtable[each_word]=doc
+                else:
+                    hashtable[each_word][docid]=doc[docid]
+                                #if word is contained in previously indexed file
 
-print hashtable
+    return hashtable, fileID_names
+
+files=enter_path()
+hashtable, fileID_to_names= create_hash(files)
+
+print files, fileID_to_names, hashtable
